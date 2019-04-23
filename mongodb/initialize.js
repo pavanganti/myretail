6f5d5449e96223cba5bb5f7f33bd5f8545997e98
pavanganti <pavanganti@gmail.com>
2019-04-23 01:00:00 -0400

    updated code for dockers

--- mongodb/initialize.js
+++ mongodb/initialize.js
@@ -1,13 +1,45 @@
-use test
-db.dropUser("myretailuser")
+use test;
+db.dropUser("myretailuser");
 db.createUser({
     user: 'myretailuser',
     pwd: 'MyRetail_pwd',
     roles: [
         {
-            role: 'readwrite',
+            role: 'readWrite',
             db: 'test'
         }
     ]
 
-})
+});
+
+db.productsprice.insertMany([
+	  {
+	    "_id" : 12345,
+	    "currentPrice":{
+	        "current_price" : 11.00,
+	        "currenycode" : "USD"
+	    }
+	    
+	  },
+	  {
+	    "_id" : 12346,
+	    "currentPrice":{
+	        "current_price" : 12.00,
+	        "currenycode" : "USD"
+	    }
+	  },
+	  {
+	    "_id" : 12347,
+	    "currentPrice":{
+	        "current_price" : 13.00,
+	        "currenycode" : "USD"
+	    }
+	  },
+	  {
+	    "_id" : 13860428,
+	    "currentPrice":{
+	        "current_price" : 14.00,
+	        "currenycode" : "USD"
+	    }
+	  }
+	]);
